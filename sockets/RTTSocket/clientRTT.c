5c61b5d00fc0e8ff246fbecf0e575fcf97568cff
cdminigun <doege.chris@yahoo.com>
2017-03-07 23:58:30 -0600

    Added RTT client / server sockets.

--- sockets/RTTSocket/clientRTT.c
+++ sockets/RTTSocket/clientRTT.c
@@ -1,3 +1,12 @@
+/************************************* clientRTT ***************************************
+ * A client socket that sends a message and the time to an echo server and then
+ * accepts the return packets within a multithreaded function and calculates the
+ * time that it took for the packet to calculate the average round trip time.
+ *
+ * Note that due to limitations, this program can only be utilized within a linux
+ * environment.
+****************************************************************************************/
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <time.h>
@@ -23,81 +32,86 @@ void *recvFunc();
 int totalTime;
 int timeTaken;
 
-/* Takes input from argv  */
 int main(int argc, char *argv[])
 {
-	/* Declares variables */
-    	int i, err;
-	struct timespec curTime;
-    	char serv_ip[BUFFSIZE];
-	char sendBuf[BUFFSIZE];
-	double packetPercentage = 0.00;
-	double rttAvg = 0.00;
-	struct sockaddr_in client_addr;
-    	socklen_t server_len = sizeof(serv_addr);
+    /* Declares variables */
+    int i, err;
+    struct timespec curTime;
+    char serv_ip[BUFFSIZE];
+    char sendBuf[BUFFSIZE];
+    double packetPercentage = 0.00;
+    double rttAvg = 0.00;
+    struct sockaddr_in client_addr;
+    socklen_t server_len = sizeof(serv_addr);
 
-	strcpy(serv_ip, argv[1]);
+    strcpy(serv_ip, argv[1]);
 
-	/*  If there aren't enough inputs for argv, exit. */
-    	if(argc != 2)
-    	{
-            	printf("\n Usage: %s <ip of server> \n",argv[0]);
-            	return 1;
+    /*  If there aren't enough inputs for argv, exit. */
+    if(argc != 2)
+    {
+        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        return -1;
+    }
+
+
+    /* Fills out the server and client with family, port and ip address.  */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(TARGET_PORT);
+    serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(0);
+    client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    /* Creates a UDP socket. */
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    {
+        printf("An erorr occured with the creation of the socket.");
+        exit(-1);
+    }
+
+    /* Binds the socket to the client's INADDR_ANY and selects a random port.*/
+    if(bind(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr)) <0 ){
+        close(sockfd);
+        printf("An error occured with bind, check all inputs th client_addr");
+        exit(-1);
+    }
+
+    err = pthread_create(&tid, NULL, recvFunc, NULL);
+    if (err != 0)
+        printf("\nCan't create thread::[%s]", strerror(err));
+    for(i=0;i<1000000;i++){
+        /* Sends the designated data to the IP Address designated by user input from ARGC/ARGV. */
+        clock_gettime(CLOCK_MONOTONIC, &curTime);
+        if(sendto(sockfd, &curTime, sizeof(curTime), 0,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0 ){
+            printf("Error sending to serv_addr, check address and availability");
+            exit(-1);
         }
-
-
-	/* Fills out the server and client with family, port and ip address.  */
-	serv_addr.sin_family = AF_INET;
-    	serv_addr.sin_port = htons(TARGET_PORT);
-    	serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
-    	client_addr.sin_family = AF_INET;
-	client_addr.sin_port = htons(0);
-	client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-
-	/* Creates a UDP socket. */
-    	if((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-    	{
-        	printf("An erorr occured with the creation of the socket.");
-		exit(-1);
-	}
-
-	/* Binds the socket to the client's INADDR_ANY and selects a random port.*/
-	if(bind(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr)) <0 ){
-		close(sockfd);
-		printf("An error occured with bind, check all inputs th client_addr");
-		exit(-1);
-	}
-
-	err = pthread_create(&tid, NULL, recvFunc, NULL);
-	if (err != 0)
-		printf("\nCan't create thread::[%s]", strerror(err));
-	for(i=0;i<1000000;i++){
-		/* Sends the designated data to the IP Address designated by user input from ARGC/ARGV. */
-		clock_gettime(CLOCK_MONOTONIC, &curTime);
-		if(sendto(sockfd, &curTime, sizeof(curTime), 0,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0 ){
-			printf("Error sending to serv_addr, check address and availability");
-			exit(-1);
-		}
-	}
-        sleep(5);
-        rttAvg = ((double)timeTaken /1000)/(double)packetsReceived;
-        packetPercentage = (double)packetsReceived/PACKETS_SENT;
-        printf("Total Packets Received: %lf.5\n", packetPercentage*100);
-        printf("The average RTT is %lf.5 ms", rttAvg);
-	return 0;
+    }
+    sleep(5);
+    rttAvg = ((double)timeTaken /1000)/(double)packetsReceived;
+    packetPercentage = (double)packetsReceived/PACKETS_SENT;
+    printf("Total Packets Received: %lf.5\n", packetPercentage*100);
+    printf("The average RTT is %lf.5 ms", rttAvg);
+    return 0;
 }
 
+/***************************void *recvFunc*********************
+ * A multi-threaded function that opens a thread to accept
+ * communications from a previously initiated packet.
+ *
+ * Does not return.
+ *
+ ************************************************************/
 void *recvFunc( ){
-	struct timespec curTime, oldTime;
-	packetsReceived = 0;
-	while(1)
-	{
-		if(recvfrom(sockfd, oldTime, BUFFSIZE, 0, (struct sockaddr*)&serv_addr, 0)){
-			clock_gettime(CLOCK_MONOTONIC, &curTime);
-			timeTaken += (curTime.tv_sec - oldTime.tv_sec) * 1000000 + (curTime.tv_nsec - oldTime.tv_nsec) / 1000);
-			packetsReceived++;
-				
-		}
-	}
+    struct timespec curTime, oldTime;
+    packetsReceived = 0;
+    while(1)
+    {
+        if(recvfrom(sockfd, oldTime, BUFFSIZE, 0, (struct sockaddr*)&serv_addr, 0)){
+            clock_gettime(CLOCK_MONOTONIC, &curTime);
+            timeTaken += (curTime.tv_sec - oldTime.tv_sec) * 1000000 + (curTime.tv_nsec - oldTime.tv_nsec) / 1000);
+            packetsReceived++;
+        }
+    }
 }
--- sockets/RTTSocket/serverRTT.c
+++ sockets/RTTSocket/serverRTT.c
@@ -1,4 +1,3 @@
-nclude <pthread.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,44 +13,42 @@ nclude <pthread.h>
 #define BUFFSIZE 90
 
 int main(int argc, char *argv[]){
+    /* Declared variables. */
+    int listenfd = 0, connfd = 0, n=0, size;
+    struct sockaddr_in serv_addr;
+    char recvBuff[BUFFSIZE];
 
+    /* Binds the UDP socket to the listenfd*/
+    if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        printf("Socket Error");
+        exit(-1);
+    }
+    /*Zeros out the struct and buffer */
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    /*Fills the serv_addr struct with information needed for binding */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(TARGET_PORT);
+    /* Sets the port and interface for the server to listen on*/
+    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) <0){
+        close(listenfd);
+        printf("An issue occured with bind, check the struct and inputs");
+        exit(-1);
+    }
+    /* Sets a maxium of 10 corruent connections */
+    listen(listenfd, 10);
 
-	/* Declared variables. */
-    	int listenfd = 0, connfd = 0, n=0, size;
-    	struct sockaddr_in serv_addr;
-    	char recvBuff[BUFFSIZE];
-
-	/* Binds the UDP socket to the listenfd*/
-    	if((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-	    	printf("Socket Error");
-		exit(-1);
-    	}
-	/*Zeros out the struct and buffer */
-	memset(&serv_addr, '0', sizeof(serv_addr));
-	/*Fills the serv_addr struct with information needed for binding */
-    	serv_addr.sin_family = AF_INET;
-    	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    	serv_addr.sin_port = htons(TARGET_PORT);
-	/* Sets the port and interface for the server to listen on*/
-    	if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) <0){
-		close(listenfd);
-		printf("An issue occured with bind, check the struct and inputs");
-	      	exit(-1);
-    	}
-	/* Sets a maxium of 10 corruent connections */
-    	listen(listenfd, 10);
-
-	/* Continiously waits for input from the socket.*/
-    	printf("Server starting...waiting for input.\n");
-	while(1)
-    	{
-		/* Receives input from any client connection to this IP address
-		 * (INADDRY_ANY is bound to all interfaces, 127.0.0.1 and any
-		 * ip address on a NIC. Doesn't error out on bad input, however
-		 * it doesn't print out bad inputs. */
-		if(recvfrom(listenfd, recvBuff, BUFFSIZE , 0, (struct sockaddr*)&serv_addr,0) > 0){
-			printf("%s", recvBuff);
-		}
-    	sleep(1);
-    	}
+    /* Continiously waits for input from the socket.*/
+    printf("Server starting...waiting for input.\n");
+    while(1)
+    {
+        /* Receives input from any client connection to this IP address
+         * (INADDRY_ANY is bound to all interfaces, 127.0.0.1 and any
+         * ip address on a NIC. Doesn't error out on bad input, however
+         * it doesn't print out bad inputs. */
+        if(recvfrom(listenfd, recvBuff, BUFFSIZE , 0, (struct sockaddr*)&serv_addr,0) > 0){
+            printf("%s", recvBuff);
+        }
+        sleep(1);
+    }
 }
